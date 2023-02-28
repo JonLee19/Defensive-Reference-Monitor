2abf8196566ae7e4d78bc2801c30647e59681b22
JonLee19 <42459446+JonLee19@users.noreply.github.com>
2023-02-27 18:54:23 -0800

    Update run script for test 1

--- repy_v2/RUNNABLE/run_pt1.sh
+++ repy_v2/RUNNABLE/run_pt1.sh
@@ -11,13 +11,17 @@ RUNNABLE="$REPY_V2/RUNNABLE"
 RUN_REPY="$RUNNABLE/repy.py"
 RESTRICTIONS="$RUNNABLE/tutorial/restrictions.test"
 ENCASEMENTLIB="$RUNNABLE/encasementlib.r2py"
-# DEFENSE="$RUNNABLE/defense/defensive_reference_monitor.r2py"
-# OFFENSE="$RUNNABLE/offense/attackcase0.r2py"
-DEFENSE1=defensive_reference_monitor1.r2py
-OFFENSE0=attackcase0.r2py
-OFFENSE1=attackcase1.r2py
-OFFENSE2=attackcase2.r2py
+DEFENSE_DIR="$RUNNABLE/defense"
+OFFENSE_DIR="$RUNNABLE/offense"
 
+DEFENSE1="$DEFENSE_DIR/defensive_reference_monitor1.r2py"
+
+OFFENSE0="$OFFENSE_DIR/attackcase0.r2py"
+OFFENSE1="$OFFENSE_DIR/attackcase1.r2py"
+OFFENSE2="$OFFENSE_DIR/attackcase2.r2py"
+
+DEFENSE="defensive_reference_monitor.r2py"
+OFFENSE="attackcase.r2py"
 
 #echo "Activate Venv"
 #$activate #doesn't work through a bash shell rather than this parent terminal
@@ -25,11 +29,20 @@ OFFENSE2=attackcase2.r2py
 echo "Test Tutorial:"
 python "$RUN_REPY" "$RUNNABLE/tutorial/restrictions.test" "$RUNNABLE/tutorial/example.1.1.r2py"
 
-echo "Test Example 0:"
-python "$RUN_REPY" "$RESTRICTIONS" "$ENCASEMENTLIB" "$DEFENSE1" "$OFFENSE0"
+echo "Cleanup Previous Files"
+rm -f "$DEFENSE" "$OFFENSE"
 
-echo "Test Example 1:"
-python "$RUN_REPY" "$RESTRICTIONS" "$ENCASEMENTLIB" "$DEFENSE1" "$OFFENSE1"
+echo "Copying Defensive Layer"
+cp "$DEFENSE1" "$DEFENSE"
 
-echo "Test Example 2:"
-python "$RUN_REPY" "$RESTRICTIONS" "$ENCASEMENTLIB" "$DEFENSE1" "$OFFENSE2"
+echo "|Test Example 0:|"
+cp "$OFFENSE0" "$OFFENSE" #Copying Attack 0
+python "$RUN_REPY" "$RESTRICTIONS" "$ENCASEMENTLIB" "$DEFENSE" "$OFFENSE"
+
+echo "|Test Example 1:|"
+cp "$OFFENSE1" "$OFFENSE" #Copying Attack 1
+python "$RUN_REPY" "$RESTRICTIONS" "$ENCASEMENTLIB" "$DEFENSE" "$OFFENSE"
+
+echo "|Test Example 2:|"
+cp "$OFFENSE2" "$OFFENSE" #Copying Attack 2
+python "$RUN_REPY" "$RESTRICTIONS" "$ENCASEMENTLIB" "$DEFENSE" "$OFFENSE"
