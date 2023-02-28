0a443194710b2481a95fe836a7cbb1197b24257b
JonLee19 <42459446+JonLee19@users.noreply.github.com>
2023-02-27 19:10:03 -0800

    Moving run script test 1 to run scripts folder

--- repy_v2/RUNNABLE/run_scripts/run_test1.sh
+++ repy_v2/RUNNABLE/run_scripts/run_test1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #run with command: 
 #sh 'C:/Users/leejo/Personal/Coding Projects/AB reference monitor/repy_v2/RUNNABLE/run_scripts/run_pt1.sh'
+#sh 'C:/Users/leejo/Personal/Coding Projects/AB reference monitor/repy_v2/RUNNABLE/run_pt1.sh'
 
 ACTIVATE="C:/Users/leejo/Personal/Coding Projects/AB reference monitor/venv_py2/Scripts/activate"
 
@@ -9,49 +10,43 @@ RUNNABLE="$REPY_V2/RUNNABLE"
 RUN_REPY="$RUNNABLE/repy.py"
 RESTRICTIONS="$RUNNABLE/tutorial/restrictions.test"
 ENCASEMENTLIB="$RUNNABLE/encasementlib.r2py"
-DEFENSE="$RUNNABLE/defense/defensive_reference_monitor.r2py"
-OFFENSE="$RUNNABLE/offense/attackcase0.r2py"
 
+# Defense
+DEFENSE_DIR="$RUNNABLE/defense"
+DEFENSE1="$DEFENSE_DIR/defensive_reference_monitor1.r2py"
 
-echo "Activate Venv"
-$activate #doesn't work through a bash shell rather than this parent terminal
+# Offense
+OFFENSE_DIR="$RUNNABLE/offense"
+OFFENSE0="$OFFENSE_DIR/attackcase0.r2py"
+OFFENSE1="$OFFENSE_DIR/attackcase1.r2py"
+OFFENSE2="$OFFENSE_DIR/attackcase2.r2py"
+
+# Generic to run the same script
+DEFENSE="defensive_reference_monitor.r2py"
+OFFENSE="attackcase.r2py"
+
+#echo "Activate Venv"
+#$activate #doesn't work through a bash shell rather than this parent terminal
+
+cd "$RUNNABLE"
 
 echo "Test Tutorial:"
 python "$RUN_REPY" "$RUNNABLE/tutorial/restrictions.test" "$RUNNABLE/tutorial/example.1.1.r2py"
 
-echo "Test Example 1:"
-python "$RUN_REPY" "$RESTRICTIONS" "$ENCASEMENTLIB" defensive_reference_monitor.r2py attackcase0.r2py
+echo "Cleanup Previous Files"
+rm -f "$DEFENSE" "$OFFENSE"
 
-#python repy.py restrictions.test encasementlib.r2py defensive_reference_monitor0.r2py attackcase0.r2py
+echo "Copying Defensive Layer"
+cp "$DEFENSE1" "$DEFENSE"
 
+echo "|Test Example 0:|"
+cp "$OFFENSE0" "$OFFENSE" #Copying Attack 0
+python "$RUN_REPY" "$RESTRICTIONS" "$ENCASEMENTLIB" "$DEFENSE" "$OFFENSE"
 
+echo "|Test Example 1:|"
+cp "$OFFENSE1" "$OFFENSE" #Copying Attack 1
+python "$RUN_REPY" "$RESTRICTIONS" "$ENCASEMENTLIB" "$DEFENSE" "$OFFENSE"
 
-
-
-# #run with command: sh (path to this file)
-
-# REPY_V2='C:\\Users\\leejo\\Personal\\Coding\ Projects\\AB reference monitor\\repy_v2'
-# RUNNABLE="$REPY_V2\\RUNNABLE"
-
-# ACTIVATE='C:\Users\leejo\Personal\Coding Projects\AB reference monitor\venv_py2\Scripts\activate'
-
-# RUN_REPY="$RUNNABLE\\repy.py"
-
-# # echo $RUN_REPY
-# echo "Activate Venv"
-# # $activate
-
-# echo "Test Tutorial"
-# RUN='C:\Users\leejo\Personal\Coding Projects\AB reference monitor\repy_v2\RUNNABLE\repy.py'
-# echo \' $RUN \'
-# python $(realpath '$RUN')
-# #tutorial/restrictions.test tutorial/example.1.1.r2py
-
-# # echo "Test Example 1"
-# # python repy.py tutorial/restrictions.test encasementlib.r2py defensive_reference_monitor.r2py attackcase0.r2py 
-
-#NOTES
-# # python repy.py restrictions.default encasementlib.r2py [security_layer].r2py [attack_program].r2py 
-# # python ../repy.py restrictions.default encasementlib.r2py defensive_reference_monitor.r2py attackcase0.r2py 
-
-# # python ../repy.py restrictions.test example.1.1.r2py
+echo "|Test Example 2:|"
+cp "$OFFENSE2" "$OFFENSE" #Copying Attack 2
+python "$RUN_REPY" "$RESTRICTIONS" "$ENCASEMENTLIB" "$DEFENSE" "$OFFENSE"
